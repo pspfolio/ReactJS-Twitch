0358604ad5028e3c4b7801d50bb520975e2c8288
PeppeS <pspfolio@gmail.com>
2016-01-14 16:12:13 +0200

    Redux, reducer and reducer tests with chai

---
+++ src/reducers/index.jsx
@@ -0,0 +1,22 @@
+
+function items(state = {isFetching: false, items: []}, action) {
+  switch(action.type) {
+    case 'REQUEST_GAMES':
+      return Object.assign({}, state, {isFetching: true});
+    case 'RECEIVE_GAMES':
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.items
+      });
+  }
+}
+
+export function itemsByTwitch(state = {}, action) {
+  switch(action.type){
+    case 'REQUEST_GAMES':
+    case 'RECEIVE_GAMES':
+      return Object.assign({}, state, {
+        [action.twitch]: items(state[action.twitch], action)
+      });
+  }
+}
---
+++ test/reducer_spec.js
@@ -0,0 +1,77 @@
+import {expect} from 'chai';
+import {itemsByTwitch} from '../src/reducers';
+
+describe('reducer', () => {
+
+  it('handles empty state', () => {
+    const action = {
+      type: 'RECEIVE_GAMES',
+      twitch: 'TopGames',
+      items: [{Name: 'Stream1'}, {Name: 'Stream2'}]
+    };
+
+    var nextState = itemsByTwitch(undefined, action);
+    expect(nextState.TopGames.items.length).to.equal(2);
+  }),
+
+  it('handles RECEIVE_GAMES', () => {
+    const initState = {};
+    const action = {
+      type: 'RECEIVE_GAMES',
+      twitch: 'TopGames',
+      items: [{Name: 'Stream1'}, {Name: 'Stream2'}]
+    };
+
+    var nextState = itemsByTwitch(initState, action);
+    expect(nextState.TopGames.items.length).to.equal(2);
+    expect(nextState.TopGames.items[0].Name).to.equal('Stream1');
+  }),
+
+  it('RECEIVE_GAMES set isFetching to false', () => {
+    var initState = {
+      TopGames: {
+        isFetching: true,
+        items: []
+      }
+    };
+    
+    var action = {
+      type: 'RECEIVE_GAMES',
+      twitch: 'TopGames',
+      items: [{Name: 'Stream1'},{Name: 'Stream2'}]
+    };
+
+    var nextState = itemsByTwitch(initState, action);
+    expect(nextState.TopGames.isFetching).to.be.false;
+  }),
+
+  it('RECEIVE_GAMES replaces old items', () => {
+    var initState = {
+      TopGames: {
+        isFetching: false,
+        items: [{Name: 'Stream1'},{Name: 'Stream2'}]
+      }
+    };
+
+    var action = {
+      type: 'RECEIVE_GAMES',
+      twitch: 'TopGames',
+      items: [{Name: 'Stream3'},{Name: 'Stream4'}]
+    };
+
+      var nextState = itemsByTwitch(initState, action);
+      expect(nextState).not.to.be.null;
+      expect(nextState.TopGames.items[1].Name).to.equal('Stream4');
+  }),
+
+  it('handles REQUEST_GAMES', () => {
+    var initState = {};
+    const action = {
+      type: 'REQUEST_GAMES',
+      twitch: 'TopGames'
+    }
+
+    var nextState = itemsByTwitch(initState, action);
+    expect(nextState.TopGames.isFetching).to.be.true;
+  })
+});
