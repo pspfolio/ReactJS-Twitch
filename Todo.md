44704046b61f006a237216b209437279cd756ac2
PeppeS <pspfolio@gmail.com>
2016-01-15 16:19:41 +0200

    Todo MD and adding links to streams

---
+++ Todo.md
@@ -0,0 +1,5 @@
+- Datetime to items when fetched
+- Check Datetime if needed fetch new items from API / if items are null
+- Link to streams from topgames / encodeURI
+- Reducer for streams
+- Store for streams component
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Game extends Component {
+  getEncodedLink() {
+    const { game } = this.props;
+    var uri = '#/streams/' + game.name;
+    return encodeURI(uri);
+  }
   render() {
     const { game } = this.props;
+    console.log(game);
     return (
       <div className="col-xs-12 col-sm-6 col-lg-4">
         <div key={game._id}>
-          <img src={game.box.large} alt="stream twitch game" />
+          <a href={this.getEncodedLink()}><img src={game.box.large} alt="stream twitch game" /></a>
         </div>
       </div>
     )
