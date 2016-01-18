ba9ab0cd53417ea9ed06070537161b0f7f01b2a7
PeppeS <pspfolio@gmail.com>
2016-01-18 17:39:03 +0200

    Streams main component to work in basic level.

--- src/components/Streams.jsx
+++ src/components/Streams.jsx
@@ -3,12 +3,11 @@ import Stream from './Stream';
 import {connect} from 'react-redux';
 import {fetchData, fetchIfNeeded} from '../actions/actions';
 
-class Streams extends Component {
+export default class Streams extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
     const { game } = this.props.params;
     var uri = 'https://api.twitch.tv/kraken/streams?game=' + game;
-    dispatch(fetchIfNeeded(game, uri));
   }
   render() {
     return(
@@ -20,15 +19,3 @@ class Streams extends Component {
     )
   }
 }
-
-function mapStateToProps(state) {
-  const { game } = this.props.params;
-  const { isFetching, items: streams } = state.streams.game || {isFetching: false, items: []};
-
-  return {
-    isFetching,
-    streams
-  }
-}
-
-export default connect(mapStateToProps)(Streams)
