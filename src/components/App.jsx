380c2455898f988cefcfca6ac3d59573a83b85a5
PeppeS <pspfolio@gmail.com>
2016-01-20 17:20:00 +0200

    styling navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default React.createClass({
   render() {
-    return this.props.children;
+    return(
+      <div>
+        <nav>
+          <h2>Redux Twitch</h2>
+        </nav>
+        {this.props.children}
+      </div>
+    );
   }
 });
