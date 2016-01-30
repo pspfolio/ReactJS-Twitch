3b227f1bd648c8f6ea971384b4bfe781d2f90c9f
PeppeS <pspfolio@gmail.com>
2016-01-30 10:13:37 +0200

    moving dispatch call to component

--- src/components/Streams.jsx
+++ src/components/Streams.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import Stream from './Stream';
+import { fetchData } from '../actions/streams';
 
 export default class Streams extends Component {
-  render() {
-    const { streams } = this.props;
-    return(
-      <div className="row text-center">
-        {streams.map(stream =>
-          <Stream stream={stream} key={stream._id}></Stream>
-        )}
-      </div>
-    )
-  }
+    componentDidMount() {
+        const { dispatch, gameName } = this.props;
+        console.log(gameName)
+        var uri = 'https://api.twitch.tv/kraken/streams?game=' + gameName;
+        dispatch(fetchData(gameName, uri));
+    }
+    render() {
+        const { streams } = this.props;
+        return(
+            <div className="row text-center">
+                {streams.map(stream =>
+                    <Stream stream={stream} key={stream._id}></Stream>
+                )}
+            </div>
+        )
+    }
 }
--- src/containers/GameStreams.jsx
+++ src/containers/GameStreams.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import Streams from '../components/Streams';
 import {connect} from 'react-redux';
-import {fetchData} from '../actions/streams';
-
 class GameStreams extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
-    const { game } = this.props.params;
-    var uri = 'https://api.twitch.tv/kraken/streams?game=' + game;
-    dispatch(fetchData(game, uri));
-  }
   render() {
-    const { gameStreams } = this.props;
+    const { gameStreams, dispatch } = this.props;
+    const { game } = this.props.params;
     return(
-      <Streams streams={gameStreams} />
+      <Streams streams={gameStreams} dispatch={dispatch} gameName={game} />
     )
   }
 }
