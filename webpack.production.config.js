47a85bfd100dfb774f82065082b25936c41ebfa7
PeppeS <pspfolio@gmail.com>
2016-02-08 18:46:46 +0200

    fixing public path

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -25,7 +25,12 @@ resolve: {
 },
   output: {
     path: __dirname + '/prod',
-    publicPath: 'ReactJS-Twitch/prod/',
+    publicPath: '/',
     filename: 'bundle.js'
   }
 };
+
+/*
+http://pspfolio.github.io/ReactJS-Twitch/prod/12beade9232e689a16bc544b06282221.png
+ http://pspfolio.github.io/ReactJS-Twitch/prod/ReactJS-Twitch/prod/12beade9232e689a16bc544b06282221.png
+*/
